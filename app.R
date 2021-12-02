ed59a6f3fbf98131ec82055347ff2dcda90428c0
Filothei Gkavogianni <17748843+filotheig@users.noreply.github.com>
2021-12-02 10:51:28 +0200

    add raw data and a barplot

--- app.R
+++ app.R
@@ -1,10 +1,25 @@
 library(shiny)
 library(shinydashboard)
+library("readxl")
+library(DT)
+
+
+oecd<- read.csv("suicide II OECD.csv.txt", header = TRUE)
+eurostat <- read_excel("suicide1.xls",col_names =TRUE, skip=2)
+
 
 aggregate_data_oecd <- aggregate(oecd$Value, by=list(LOCATION=oecd$LOCATION), FUN=sum)
 aggregate_data_oecd_greece <- aggregate_data_oecd[aggregate_data_oecd$LOCATION=="GRC",]$x
 sum_aggregate_data_oecd <- sum(aggregate_data_oecd$x)
 aggregate_data_oecd_greece_ltu <- aggregate_data_oecd[aggregate_data_oecd$x==max(aggregate_data_oecd$x),]$x
+
+
+barplot_shiny_data <-as.matrix(t(eurostat), rownames.force = NA)
+colnames(barplot_shiny_data)<-barplot_shiny_data[1,]
+barplot_shiny_data<-barplot_shiny_data[-1,]
+class(barplot_shiny_data)<-"numeric"
+
+
 
 ui <- dashboardPage(skin = "red",
   dashboardHeader(
@@ -26,9 +41,37 @@ ui <- dashboardPage(skin = "red",
             valueBoxOutput("aggregateSuicidesEuropeValueBox"),
             valueBoxOutput("aggregateSuicidesLithuaniaValueBox"),
             valueBoxOutput("aggregateSuicidesGreeceValueBox")
+        ),
+        sidebarLayout(      
+    
+          # Define the sidebar with one input
+          sidebarPanel(
+            selectInput("Country", "Country:", 
+                        choices=colnames(barplot_shiny_data)),
+            hr()
+          ),
+          
+          # Create a spot for the barplot
+          mainPanel(
+            plotOutput("countryPlot")  
+          )
+    
         )
       ),
       tabItem("rawdata",
+        fluidRow( 
+          box(title = "Eurostat Dataset",
+              solidHeader = T,
+              width = 8,
+              collapsible = T,
+              div(DT::DTOutput("eurostat"), style = "font-size: 70%;")),
+          box(title = "Oecd Dataset",
+              solidHeader = T,
+              width = 8,
+              collapsible = T,
+              div(DT::DTOutput("oecd"), style = "font-size: 70%;"))
+
+        )
         # numericInput("maxrows", "Rows to show", 25),
         # verbatimTextOutput("rawtable"),
         # downloadButton("downloadCsv", "Download as CSV")
@@ -66,6 +109,22 @@ server <- function(input, output, session) {
             color = "aqua"
         )
     })
+
+    # Fill in the spot we created for a plot
+    output$countryPlot <- renderPlot({
+      
+      # Render a barplot
+      barplot(barplot_shiny_data[,input$Country],
+        main=input$Country,
+              ylab="Number of Suicides",
+              xlab="Year")
+    })
+
+    output$eurostat <- DT::renderDataTable(eurostat,
+                                       rownames=F, options = list(pageLength = 10))
+    output$oecd <- DT::renderDataTable(oecd,
+                                       rownames=F, options = list(pageLength = 15))                                    
+
 }
 
 shinyApp(ui = ui, server = server)
